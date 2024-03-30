4d574b535f63ce676478ebc444d454d4cf1fbac6
linglengfeng <1027322856@qq.com>
2024-03-30 22:57:39 +0800

    支持联合多键盘 未完成版

--- src/tocfg.go
+++ src/tocfg.go
@@ -79,14 +79,20 @@ type prop struct {
 	Val  any
 }
 
-type keyInfo struct {
-	Key  string
-	Cell int
+type PrimarykeyVal struct {
+	key valInfo
+	val []valInfo
 }
 
-type keyVal struct {
-	Key any
-	Val any
+type valInfo struct {
+	Name string
+	Type string
+	Val  string
+}
+
+type UnionKeysVals struct {
+	Keys []valInfo
+	Vals [][]valInfo
 }
 
 var (
@@ -96,12 +102,12 @@ var (
 	globalUnionKeys  = prop{Type: UNION_KEYS, Raw: UNION_KEYS_RAW, Cell: UNION_KEYS_CELL, Name: UNION_KEYS_NAME}
 	globalOutSvr     = prop{Type: OUT_SVR, Raw: OUT_SVR_RAW, Cell: OUT_SVR_CELL}
 	globalOutCli     = prop{Type: OUT_CLI, Raw: OUT_CLI_RAW, Cell: OUT_CLI_CELL}
-	globalTypeProp   = prop{Type: TYPE, Raw: TYPE_RAW, Cell: TYPE_CELL}
+	globalType       = prop{Type: TYPE, Raw: TYPE_RAW, Cell: TYPE_CELL}
 	globalName       = prop{Type: NAME, Raw: NAME_RAW, Cell: NAME_CELL}
 	globalKey        = prop{Type: KEY, Raw: KEY_RAW, Cell: KEY_CELL}
 	globalNote       = prop{Type: NOTE, Raw: NOTE_RAW, Cell: NOTE_CELL}
 
-	globalCheckList = []prop{globalExportSvr, globalExportCli, globalPrimaryKey, globalUnionKeys, globalOutSvr, globalOutCli, globalTypeProp, globalName, globalKey, globalNote}
+	globalCheckList = []prop{globalExportSvr, globalExportCli, globalPrimaryKey, globalUnionKeys, globalOutSvr, globalOutCli, globalType, globalName, globalKey, globalNote}
 
 	globalIntDefault  = 0
 	globalStrDefault  = ""
@@ -113,6 +119,9 @@ var (
 	globalOutDir      = ""
 	globalSvrFilePath = ""
 	globalCliFilePath = ""
+
+	globalPrimaryKeyInfo = valInfo{}
+	globalUnionKyesInfo  = []valInfo{}
 
 	globalTojsonWriter = new(tojson.Writer)
 )
@@ -160,33 +169,61 @@ func startSingle(fileName string) {
 			return
 		}
 		setPropVal(excelFileName, sheet.Name, sheet.Rows)
-
 		svrMapInfo := map[string]any{}
 		cliMapInfo := map[string]any{}
-		for _, row := range sheet.Rows {
+		svrSliceInfo := []PrimarykeyVal{}
+		cliSliceInfo := []PrimarykeyVal{}
+		for rowIndex, row := range sheet.Rows {
+			if rowIndex == globalKey.Raw {
+				for cellIndex, cell := range row.Cells {
+					text := cell.String()
+					if text == globalPrimaryKey.Val {
+						globalPrimaryKeyInfo.Name = text
+						globalPrimaryKeyInfo.Val = text
+						globalPrimaryKeyInfo.Type = sheet.Rows[globalType.Raw].Cells[cellIndex].String()
+					}
+
+				}
+			}
 			if len(row.Cells) <= 0 || row.Cells[VALUE_CELL].String() != VALUE {
 				continue
 			}
 
 			addSvrMap := map[string]any{}
 			addCliMap := map[string]any{}
+			addSvrSlice := []valInfo{}
+			addCliSlice := []valInfo{}
 			for cellIndex, cell := range row.Cells {
 				text := cell.String()
-				key := sheet.Rows[globalKey.Raw].Cells[cellIndex].String()
+				addkeyName := sheet.Rows[globalKey.Raw].Cells[cellIndex].String()
+				addkeyType := sheet.Rows[globalType.Raw].Cells[cellIndex].String()
 				if sheet.Rows[globalOutSvr.Raw].Cells[cellIndex].String() == TRUE {
-					addSvrMap[key] = text
+					addSvrMap[addkeyName] = text
+					addSvrSlice = append(addSvrSlice, valInfo{Type: addkeyType, Val: text, Name: addkeyName})
 				}
 				if sheet.Rows[globalOutCli.Raw].Cells[cellIndex].String() == TRUE {
-					addCliMap[key] = text
+					addCliMap[addkeyName] = text
+					addCliSlice = append(addCliSlice, valInfo{Type: addkeyType, Val: text, Name: addkeyName})
 				}
 				// fmt.Printf("(%d,%d):%s\t", rowIndex, cellIndex, text)
 				// fmt.Printf("%s\t", text)
 			}
 			// fmt.Println()
-			idKey := row.Cells[VALUE_CELL+1].String()
-			svrMapInfo[idKey] = addSvrMap
-			cliMapInfo[idKey] = addSvrMap
+			idVal := row.Cells[VALUE_CELL+1].String()
+			idType := sheet.Rows[globalType.Raw].Cells[VALUE_CELL+1].String()
+			idName := sheet.Rows[globalName.Raw].Cells[VALUE_CELL+1].String()
+			// svrMapInfo[idKey] = addSvrMap
+			// cliMapInfo[idKey] = addSvrMap
+			svrSliceInfo = append(svrSliceInfo, PrimarykeyVal{key: valInfo{Type: idType, Val: idVal, Name: idName}, val: addSvrSlice})
+			cliSliceInfo = append(cliSliceInfo, PrimarykeyVal{key: valInfo{Type: idType, Val: idVal, Name: idName}, val: addCliSlice})
 		}
+		// globalPrimaryKey
+		fmt.Println("====================== globalPrimaryKey:", globalPrimaryKey)
+		fmt.Println("====================== globalUnionKeys:", globalUnionKeys)
+		fmt.Println("====================== globalPrimaryKeyInfo:", globalPrimaryKeyInfo)
+		fmt.Println("====================== globalUnionKeyInfo:", globalUnionKyesInfo)
+		fmt.Println("====================== svrSliceInfo:", svrSliceInfo)
+		fmt.Println("====================== cliSliceInfo:", cliSliceInfo)
 
 		globalTojsonWriter.SvrNameStr = fmt.Sprintf("%v", globalExportSvr.Val)
 		globalTojsonWriter.CliNameStr = fmt.Sprintf("%v", globalExportCli.Val)
@@ -195,11 +232,12 @@ func startSingle(fileName string) {
 		globalTojsonWriter.SvrData = svrMapInfo
 		globalTojsonWriter.CliData = cliMapInfo
 
-		err = write()
-		if err != nil {
-			fmt.Println("file:", excelFileName, "sheet name:", sheet.Name, "write file error:", err)
-			return
-		}
+		generateUnionKeysInfo(svrSliceInfo, cliSliceInfo)
+		// err = write()
+		// if err != nil {
+		// 	fmt.Println("file:", excelFileName, "sheet name:", sheet.Name, "write file error:", err)
+		// 	return
+		// }
 
 		fmt.Println("file:", excelFileName, "sheet name:", sheet.Name, "ok.")
 	}
@@ -250,7 +288,7 @@ func setPropVal(fileName string, sheetName string, rows []*xlsx.Row) {
 			cells := rows[prop.Raw].Cells
 			name := ""
 			if len(cells)-1 >= prop.Name {
-				name = fmt.Sprintf("%v", rows[prop.Raw].Cells[prop.Name])
+				name = rows[prop.Raw].Cells[prop.Name].String()
 			} else {
 				name = fileName + "_" + sheetName
 			}
@@ -259,11 +297,15 @@ func setPropVal(fileName string, sheetName string, rows []*xlsx.Row) {
 			cells := rows[prop.Raw].Cells
 			name := ""
 			if len(cells)-1 >= prop.Name {
-				name = fmt.Sprintf("%v", rows[prop.Raw].Cells[prop.Name])
+				name = rows[prop.Raw].Cells[prop.Name].String()
 			} else {
 				name = fileName + "_" + sheetName
 			}
 			globalExportCli.Val = name
+		case PRIMARY_KEY:
+			globalPrimaryKey.Val = rows[prop.Raw].Cells[prop.Name].String()
+		case UNION_KEYS:
+			globalUnionKeys.Val = rows[prop.Raw].Cells[prop.Name].String()
 		default:
 			continue
 		}
@@ -282,4 +324,179 @@ func createDir(directoryPath string) {
 			fmt.Println("create Dir:", directoryPath, "err:", err)
 		}
 	}
+}
+
+func generateUnionKeysInfo(svrSliceInfo []PrimarykeyVal, cliSliceInfo []PrimarykeyVal) {
+	unionKeys := splitKeys(fmt.Sprintf("%v", globalUnionKeys.Val))
+	fmt.Println("====================== unionKeys:", unionKeys)
+	// unionSingleMap := make(map[string][][]valInfo)              // 单个map
+	// unionInfo := make([]map[string]([][]valInfo), len(unionKeys)) //所有联合key的 map集合
+	var unionInfo []map[string]([][]valInfo)
+	for range unionKeys {
+		unionInfo = append(unionInfo, make(map[string]([][]valInfo)))
+	}
+
+	for _, svrSliceInfoSingle := range svrSliceInfo {
+
+		singleUkeys := makeSingleUkeys(unionKeys)       // 五个key ，，val 都是 svrSliceInfoSingle.val
+		singleUkeysStr := makeSingleUkeysStr(unionKeys) // 方面用map，key直接转成string
+		for _, singleVal := range svrSliceInfoSingle.val {
+			for unionKeyIndex, unionKey := range unionKeys {
+				isin, inUnionpos := inUnionKey(singleVal.Name, unionKey)
+				if isin {
+					singleUkeys[unionKeyIndex][inUnionpos] = singleVal
+					singleUkeysStr[unionKeyIndex][inUnionpos] = valInfo2Str(singleVal)
+
+				}
+			}
+		}
+
+		// keys 生成结束
+		for i, slist := range singleUkeysStr {
+			unionSingleMapVal := unionInfo[i]
+			keysOneStr := keys2Str(slist)
+			unionSingleMapVal[keysOneStr] = append(unionSingleMapVal[keysOneStr], svrSliceInfoSingle.val)
+			unionInfo[i] = unionSingleMapVal
+		}
+	}
+
+	fmt.Println("unionInfo:", unionInfo, "singleUvals ================:\n", unionInfo[2], len(unionInfo))
+
+	for i, maoinf := range unionInfo[2] {
+		fmt.Println("i:", i, "maoinf:\n", maoinf)
+	}
+
+}
+
+func makeSingleUkeys(unionKeys [][]string) [][]valInfo {
+	singleUkeys := make([][]valInfo, len(unionKeys))
+	for i, unionKey := range unionKeys {
+		singleUkeys[i] = make([]valInfo, len(unionKey))
+	}
+	return singleUkeys
+}
+
+func makeSingleUkeysStr(unionKeys [][]string) [][]string {
+	singleUkeys := make([][]string, len(unionKeys))
+	for i, unionKey := range unionKeys {
+		singleUkeys[i] = make([]string, len(unionKey))
+	}
+	return singleUkeys
+}
+
+func valInfo2Str(a valInfo) string {
+	return fmt.Sprintf("%v_%v_%v", a.Name, a.Type, a.Val)
+}
+
+func keys2Str(strlist []string) string {
+	if len(strlist) < 2 {
+		return strlist[0]
+	}
+	restr := strlist[0]
+	for _, str := range strlist[1:] {
+		restr = fmt.Sprintf("%v|%v", restr, str)
+	}
+	return restr
+}
+
+func unionKeysMaxLen(unionKeys [][]string) int {
+	max := 0
+	for _, s := range unionKeys {
+		if len(s) > max {
+			max = len(s)
+		}
+	}
+	return max
+}
+
+func inUnionKey(target string, strList []string) (bool, int) {
+	found := false
+	index := 0
+	for i, str := range strList {
+		if str == target {
+			found = true
+			index = i
+			break
+		}
+	}
+	return found, index
+}
+
+func splitKeys(str string) [][]string {
+	stack := []rune{}
+	resultKeys := [][]string{}
+	for _, char := range str {
+		if char == '[' {
+			stack = append(stack, char)
+		} else if char == ']' {
+			temp := []rune{}
+			for len(stack) > 0 && stack[len(stack)-1] != '[' {
+				temp = append(temp, stack[len(stack)-1])
+				stack = stack[:len(stack)-1]
+			}
+			if len(stack) > 0 {
+				stack = stack[:len(stack)-1]
+			}
+			if len(stack) > 0 && stack[len(stack)-1] == ',' {
+				stack = stack[:len(stack)-1]
+			}
+			resultKey := spliteString(string(temp))
+			if len(resultKey) > 0 {
+				resultKeys = append(resultKeys, resultKey)
+			}
+		} else if char == ' ' {
+
+		} else {
+			stack = append(stack, char)
+		}
+	}
+
+	return resultKeys
+}
+
+func spliteString(s string) []string {
+	result := []string{}
+	stack := []rune{}
+	for _, char := range reverseString(s) {
+		if char != ',' {
+			stack = append(stack, char)
+		} else {
+			result = append(result, string(stack))
+			stack = []rune{}
+		}
+	}
+	if len(stack) > 0 {
+		result = append(result, string(stack))
+	}
+	return result
+}
+
+func reverseString(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func Test1prop() {
+	// 初始化声明一个 map
+	myMap := map[string]int{
+		"key1": 10,
+		"key2": 20,
+	}
+
+	// 判断 map 中是否存在指定的键
+	key := "key3"
+	defaultValue := 0
+
+	if value, ok := myMap[key]; ok {
+		fmt.Printf("键 %s 的值为 %d\n", key, value)
+	} else {
+		fmt.Printf("键 %s 不存在，赋予默认值 %d\n", key, defaultValue)
+		myMap[key] = defaultValue
+	}
+
+	// 打印更新后的 map
+	fmt.Println(myMap)
 }
